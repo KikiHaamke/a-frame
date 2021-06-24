3d8201ca9af1c75dc9d4326f899d71daa7d5c6ec
KikiHaamke <79255027+KikiHaamke@users.noreply.github.com>
2021-06-24 20:24:55 +0200

    exercises made

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,21 @@
 
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+// changing individual properties with code and using setInterval
+var rotationSpeed = 0.02;
+var moveSpeed = 0.02;
+var myOtherBox = document.getElementById('myOtherBox');
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+function spin(){
+	// myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
+}
 
-// setInterval(spin, 16); //equivalent to 60 fps
+function move() {
+    myOtherBox.object3D.position.y += moveSpeed;
+    myOtherBox.object3D.position.x += moveSpeed;
+}
+
+setInterval(spin, 16); //equivalent to 60 fps
+setInterval(move, 16); //equivalent to 60 fps
